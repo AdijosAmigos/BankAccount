748f5e7b5b7af5ba336b38e2ad80d39ce2b16453
AdijosAmigos <adrian.gurda10@gmail.com>
2021-11-17 17:15:46 +0100

    test

---
+++ src/main/java/BankService.java
@@ -0,0 +1,25 @@
+import java.math.BigDecimal;
+
+public class BankService {
+
+    void transfer(Account accountFrom, Account accountTo, BigDecimal amount)  {
+        //////
+
+
+    }
+
+    public class Account {
+        private int id;
+        private BigDecimal balance;
+
+        public Account(int id, BigDecimal balance){
+
+        }
+
+    }
+
+    public BankService() {
+
+    }
+
+}
